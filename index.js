791cfab8a48399c2bcb7244a788144b93e9d456c
Sahana Deepak <sahanad149@gmail.com>
2024-08-17 21:40:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();  // Initialize app here
 
@@ -16,10 +16,12 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+console.log(process.env.MONGO_URI);
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.log('MongoDB connection error:', err));
